e1ae76ce27b9b5bde0b34c0715b0958e464119dc
Pydjama3 <79657651+Pydjama3@users.noreply.github.com>
2023-10-22 14:08:31 +0000

    Denière MAJ avant les vacances

--- mastermind/main.py
+++ mastermind/main.py
@@ -3,68 +3,134 @@ from random import randint
 # Constants and global variable
 
 color = {
-  1: ['rouge', 31],
-  2: ['jaune', 33],
-  3: ['vert', 32],
-  4: ['bleu', 34],
-  5: ['mauve', 35],
-  6: ['orange', 91],
-  7: ['rose', 95],
-  8: ['white', 37]
+    1: ['rouge', 41],
+    2: ['jaune', 93],
+    3: ['vert', 42],
+    4: ['bleu', 44],
+    5: ['mauve', 45],
+    6: ['orange', 2],
+    7: ['rose', 46],
+    8: ['blanc', 47]
 }
-class ANSI():
-    def background(code):
-        return "\33[7;49;{code}m".format(code=code)
 
+class PrintColours:
+
+  def __init__(self, string) -> None:
+    assert isinstance(string, str)
+
+    self.string = string
+    self.bg_color = 0
+    self.style = 0
+    self.tx_color = 0
+
+  def style_text(self, code):
+    self.style = code
+    return self
+
+  def color_text(self, code):
+    self.tx_color = code
+    return self
+
+  def background(self, code):
+    self.bg_color = code
+    return self
+
+  def __str__(self) -> str:
+    #return f"\33[{self.bg_color}m\33[{self.tx_color}m\33[{self.style}m" + self.string + "\33[0m"
+    return f"\33[{self.bg_color}m" + self.string + "\33[0m"
+
+# Functions
 def regles():
+  """ 
+  Cette fonction affiche les regles du Mastermind.
+  """
   print("Bienvenue dans le jeu du Mastermind !")
   print("Le but du jeu est de trouver une combinaison de 4 couleurs choisies par l'ordinateur.")
   print("Chaque couleur peut prendre les valeurs : rouge, jaune, vert, bleu, mauve, orange, rose, blanc.")
+  print("Au lieu d'entrer les couleurs a chaque fois, vous devez entrer un chiffre entre 1 et 8.")
+  print("Chaque chiffre correspond a une couleur.")
   print("Vous avez 12 essais pour trouver la bonne combinaison.")
   print("Bonne chance !")
 
-def genereCombinaison():
+def difficulte():
+  '''
+  Cette fonction demande à l'utilisateur de choisir un niveau de difficulté.
+  Returns:
+    nd (int) : le numero correspondant à la difficultée (facile = 3, moyen = 4, difficile = 5)
+  '''
+  nd = ""
+  while nd != "facile" and nd != "moyen" and nd != "difficile":
+    nd = input("\nQuelle difficultée voulez vous ? (facile/moyen/difficile) : ").lower()
+  if nd == "facile":
+    nd = 3
+  elif nd == "moyen":
+    nd = 4
+  else:
+    nd = 5
+  return nd
+
+def genere_combinaison(nd=4):
   """
   Cette fonction génère une combinaison de 4 chiffres choisis au hasard entre 1 et 8.
-  @author Florence <3
+  Paramètre:
+    nd (int) : la longueur de la combinaison generer
+  Returns: 
+    combinaison (list) : la combinaison de couleur de longueur nd
   """
   combinaison = []
-  for _ in range(4):
-    combinaison.append(randint(1,8))
+  for _ in range(nd):
+    combinaison.append(randint(1, 8))
   return combinaison
 
-def enterProposition():
+def entrer_proposition(nbEssai, nd=4):
   """
-  Cette fonction demande à l'utilisateur de saisir une combinaison de 4 chiffres choisis entre
-  1 et 8.
+  Cette fonction demande à l'utilisateur de saisir une combinaison de 4 chiffres choisis
+  entre 1 et 8.
+  Paramètre:
+    nbEssai (int) : le numero de l'essai de l'utilisateur
+    nd (int) : la longueur de la proposition que l'utilisateur doit entrer
+  Returns:
+    proposition (list) : les 4 propositions que l'utilisateur veut tester
   """
   proposition = []
-  for _ in range(4):
+  print("\n\033[1mEssai numero", nbEssai+1,"\033[0m")
+  for _ in range(nd):
     num = input("Entrez une couleur : ")
-    if len(num) == 0:
-      num = int(input("Re-entrez une couleur entre 1 et 8: "))  
-    while len(str(num)) > 1 or str(num).isalpha() is True:
-      num = int(input("Re-entrez une couleur entre 1 et 8: "))
-    num = int(num)
-    while num < 1 or num > 8:
-      num = int(input("Re-entrez une couleur entre 1 et 8: "))
+    while isinstance(num, int) is False:
+      try:
+        num = int(num)
+        if num < 1 or num > 8:
+          num = input("\033[1ARe-entrez une couleur : \033[K")
+          continue
+      except ValueError:
+        num = input("\033[1ARe-entrez une couleur : \033[K")
+        continue
     proposition.append(num)
   return proposition
 
-def nbCouleurBp(combinaison, proposition):
+def nb_couleur_Bp(combinaison, proposition):
   """
-  Verfifie lesquels des nombres de couleurs sont bien placés.
-  @author Florence <3
+  Cette fonction compte le nombre de couleurs bien placées.
+  Paramètres:
+    combinaison (list) : la combinaison de couleur
+    proposition (list) : les 4 propositions que l'utilisateur veut tester
+  Returns:
+    count (int) : le nombre de couleurs bien placées 
   """
   count = 0
-  for i in range(4):
+  for i in range(len(combinaison)):
     if combinaison[i] == proposition[i]:
       count += 1
   return count
 
-def nbCouleurMp(combinaison, proposition):
+def nb_couleur_Mp(combinaison, proposition):
   """
   Cette fonction compte le nombre de couleurs mal placées.
+  Paramètres:
+    combinaison (list) : la combinaison de couleur
+    proposition (list) : les 4 propositions que l'utilisateur veut tester
+  Returns:
+    nb (int) : le nombre de couleurs mal placées 
   """
   nb = 0
   dCombinaison = {}
@@ -89,30 +155,42 @@ def nbCouleurMp(combinaison, proposition):
       else:
         dResult[color] = dCombinaison[color]
 
-  nb=sum(dResult.values())
+  nb = sum(dResult.values())
   return nb
 
 def jeu():
   """
   Cette fonction gère le jeu en lui même.
   """
-  combinaison = genereCombinaison()
-  nbEssais = 0
-  gagne = False
-  while nbEssais < 12:
-    print(ANSI.background(color[1][1]) + str(combinaison)+"]")
-    proposition = enterProposition()
-    nbBp = nbCouleurBp(combinaison, proposition)
-    if nbBp == 4:
-      print("Vous avez gagné!!")
-      gagne = True
-      break
-    nbMp = nbCouleurMp(combinaison, proposition) - nbBp
-    print("Vous avez trouvé", nbBp, "couleurs bien placées.")
-    print("Vous avez trouvé", nbMp, "couleurs mal placées.")
-  if gagne is False: 
-    print("Perdu!")
-  
+  jouer = True
+  regles()
+  nd = difficulte()
+  while jouer:
+    combinaison = genere_combinaison(nd)
+    gagne = False
+    for nbEssai in range(12):
+      #printColours(combinaison)
+      proposition = entrer_proposition(nbEssai, nd)
+      nbBp = nb_couleur_Bp(combinaison, proposition)
+      if nbBp == nd:
+        print("\n\033[1mVous avez gagné en", nbEssai+1, "essais!\033[0m")
+        gagne = True
+        break
+      nbMp = nb_couleur_Mp(combinaison, proposition) - nbBp
+      print("Vous avez trouvé", nbBp, "couleurs bien placées.")
+      print("Vous avez trouvé", nbMp, "couleurs mal placées.")
+    if not gagne:
+      print("\n\033[1mVous avez perdu!\033[0m")
+    if input("Voulez vous rejouez? (oui/non) : ").lower() == "non":
+      jouer = False
+
+def indexToANSI(t, bg=0, st=0, co=0):
+  ansi = PrintColours(t).background(bg).color_text(co).style_text(st)
+  return str(ansi)
+
+def printColours(colourList):
+  colourful = [indexToANSI(str(e), bg=color[e][1]) for e in colourList]
+  print(indexToANSI("1", bg=1))
 
 if __name__ == "__main__":
   jeu()
